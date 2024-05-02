fc34e123371c274b197c6e0b2511d89135f6aa37
Atabek200 <akylbekovatabek564@gmail.com>
2024-05-02 18:30:56 +0600

    asdlajhg

--- academy/urls.py
+++ academy/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import MyView, MyFileView
 
 urlpatterns = [
-    path('my-view/', login_required(MyView.as_view())),
+    path('my-view1/', login_required(MyView.as_view())),
     path('api/myfiles/', MyFileView.as_view(), name='myfiles'),
 ]
